f8061e8147b1e9857fb74ba91cbaabb6008ae74e
Shawn <760243212@qq.com>
2021-07-16 14:16:08 +0800

    create Admin

--- typeidea/blog/admin.py
+++ typeidea/blog/admin.py
@@ -1,3 +1,97 @@
 from django.contrib import admin
+from django.urls import reverse
+from django.utils.html import format_html
 
-# Register your models here.
+from .models import Post, Category, Tag
+from .adminforms import PostAdminForm
+from typeidea.custom_site import custom_site
+from typeidea.base_admin import BaseOwnerAdmin
+
+
+class PostInline(admin.TabularInline):
+    fields = ('title', 'desc',)
+    extra = 1
+    model = Post
+
+
+@admin.register(Category, site=custom_site)
+class CategoryAdmin(BaseOwnerAdmin):
+    inlines = [PostInline]
+    list_display = (
+        'name', 'status', 'is_nav',
+        'created_time', 'post_count'
+    )
+    fields = ('name', 'status', 'is_nav')
+
+    def post_count(self, obj):
+        return obj.post_set.count()
+    post_count.short_description = '文章数量'
+
+
+@admin.register(Tag, site=custom_site)
+class TagAdmin(BaseOwnerAdmin):
+    list_display = ('name', 'status', 'created_time')
+    fields = ('name', 'status')
+
+
+class CategoryOwnerFilter(admin.SimpleListFilter):
+    title = '分类过滤器'
+    parameter_name = 'owner_category'
+
+    def lookups(self, request, model_admin):
+        return Category.objects.filter(owner=request.user).values_list('id', 'name')
+
+    def queryset(self, request, queryset):
+        category_id = self.value()
+        if category_id:
+            return queryset.filter(category_id=self.value())
+        return queryset
+
+
+@admin.register(Post, site=custom_site)
+class PostAdmin(BaseOwnerAdmin):
+    form = PostAdminForm
+
+    exclude = ('owner',)
+
+    list_display = [
+        'title', 'category', 'status',
+        'created_time', 'operator',
+    ]
+
+    list_display_links = []
+    list_filter = [CategoryOwnerFilter]
+    search_fields = ['title', 'category__name']
+
+    actions_on_top = True
+    actions_on_bottom = True
+
+    save_on_top = True
+
+    fieldsets = (
+        ('基础配置', {
+            'description': '基础配置描述',
+            'fields': (
+                ('title', 'category'),
+                'status',
+            )
+        }),
+        ('内容', {
+            'fields': (
+                'desc',
+                'content',
+            )
+        }),
+        ('额外信息', {
+            'classes': ('collapse',),
+            'fields': ('tag',),
+        })
+    )
+
+    def operator(self, obj):
+        return format_html(
+            '<a href="{}">编辑</a>',
+            reverse('cus_admin:blog_post_change', args=('obj.id',))
+        )
+
+    operator.short_description = '操作'
