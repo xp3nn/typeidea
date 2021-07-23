475051a95343aab438bc8c99bf50ea8bb13140c2
Penn <760243212@qq.com>
2021-07-23 19:28:12 +0800

    Daily Update

--- typeidea/typeidea/urls.py
+++ typeidea/typeidea/urls.py
@@ -16,9 +16,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from blog.views import post_list, post_detail
+from config.views import links
 from typeidea.custom_site import custom_site
 
 urlpatterns = [
+    path('', post_list),
+    path('category/<int:category_id>/', post_list),
+    path('tag/<int:tag_id>/', post_list),
+    path('post/<int:post_id>/', post_detail),
+    path('links/', links),
     path('admin/', custom_site.urls),
     path('super_admin/', admin.site.urls)
 ]
